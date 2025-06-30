b6ef94360e26e4572aad3decf5c279e573e7080d
DEENABE <deenathayalan9039@gmail.com>
2025-06-30 13:36:12 +0530

    update

--- Frontend/src/form/Signup.jsx
+++ Frontend/src/form/Signup.jsx
@@ -41,7 +41,7 @@ const Signup = () => {
 
     try {
       const response = await axios.post(
-        "https:/login-uo0t.onrender.com/auth/signup",
+        "https://login-uo0t.onrender.com/auth/signup",
         formData
       );
       console.log("✅ Backend Response:", response.data);
